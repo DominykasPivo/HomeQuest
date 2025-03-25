6648b469a0bba337cb8de37088e94af012a9760a
Dom <domas.pivoras@gmail.com>
2025-03-25 21:14:06 +0100

    Workshop task 2 weeks ago
    This is done for the workshop 2 weeks ago in person

--- HomeQuest/myapp/views.py
+++ HomeQuest/myapp/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import UserInput
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello world!")
 
-
 def input_form(request):
     if request.method == 'POST':
         user_input = request.POST.get('user_input')
         UserInput.objects.create(text=user_input)
         return redirect('input_form')
-    return render(request, 'base.html')
+
+    # Retrieve all UserInput entries
+    user_inputs = UserInput.objects.all()
+    context = {'user_inputs': user_inputs}
+    return render(request, 'base.html', context)
 
