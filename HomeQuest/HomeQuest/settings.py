3accd4669dbfdf1cf352532b99da592b20696db1
Valentin Toscano <valentin.toscano@hotmail.com>
2025-05-01 21:41:25 +0200

    Implemented the styles for home, and property details and list

--- HomeQuest/HomeQuest/settings.py
+++ HomeQuest/HomeQuest/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "myapp" / "static",
 ]
-if DEBUG:
-    STATICFILES_DIRS.append(BASE_DIR / "myapp" / "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
