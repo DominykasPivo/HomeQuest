54b720dcb5b32268fedaf746724d643b6636b1d7
Valentin Toscano <valentin.toscano@hotmail.com>
2025-05-02 20:15:25 +0200

    Just cleaning up

--- HomeQuest/HomeQuest/settings.py
+++ HomeQuest/HomeQuest/settings.py
@@ -124,8 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-if DEBUG:
-    STATICFILES_DIRS.append(BASE_DIR / "myapp" / "static")
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
